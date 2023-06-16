3337ca1f1d97a608bf077cc1a3d601741477049e
bhangalepratik <pratikbhangale760@gmail.com>
2023-06-16 15:01:26 +0100

    called REST apis

--- src/NewTaskForm/NewTaskForm.js
+++ src/NewTaskForm/NewTaskForm.js
@@ -3,7 +3,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import PropTypes from "prop-types";
-import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
+import filterTask from "../Helpers/filterTask";
+
+("src/Helpers/filterTask.js");
 
 function NewTaskForm(props) {
   const { tasks, setTasks, setShowForm, task, setTask } = props;
@@ -12,25 +15,53 @@ function NewTaskForm(props) {
     setTask({ ...task, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     // task is already present  in task list so no need to add the task, we just need to update it and push it.
     if (task.id) {
-      setTasks(tasks.map((t) => (t.id === task.id ? task : t)));
+      axios
+        .patch("http://localhost:8080/tasks", task, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
+          },
+        })
+        .then((response) => {
+          const backendTask = response.data;
+
+          setTask(() => filterTask(backendTask));
+          setTasks(tasks.map((t) => (t.id === task.id ? task : t)));
+          setTask({
+            id: "",
+            date: "",
+            time: "",
+            title: "",
+            details: "",
+          });
+          setShowForm(false);
+        });
     } else {
       // task is added newly, assign id to it and add in task list
-      task.id = uuidv4();
-      setTasks([...tasks, task]);
-    }
+      axios
+        .post("http://localhost:8080/tasks", task, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
+          },
+        })
+        .then((response) => {
+          const backendTask = response.data;
 
-    setTask({
-      id: "",
-      date: "",
-      time: "",
-      title: "",
-      details: "",
-    });
-    setShowForm(false);
+          setTask(() => filterTask(backendTask));
+          setTasks([...tasks, task]);
+          setTask({
+            id: "",
+            date: "",
+            time: "",
+            title: "",
+            details: "",
+          });
+          setShowForm(false);
+        });
+    }
   };
 
   const handleReset = () => {
@@ -91,25 +122,24 @@ function NewTaskForm(props) {
     </>
   );
 }
-
 NewTaskForm.propTypes = {
   tasks: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
-      date: PropTypes.string.isRequired,
-      time: PropTypes.string.isRequired,
+      date: PropTypes.string,
+      time: PropTypes.string,
       title: PropTypes.string.isRequired,
-      details: PropTypes.string.isRequired,
+      details: PropTypes.string,
     })
   ).isRequired,
   setTasks: PropTypes.func.isRequired,
   setShowForm: PropTypes.func.isRequired,
   task: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    date: PropTypes.string.isRequired,
-    time: PropTypes.string.isRequired,
+    id: PropTypes.string,
+    date: PropTypes.string,
+    time: PropTypes.string,
     title: PropTypes.string.isRequired,
-    details: PropTypes.string.isRequired,
+    details: PropTypes.string,
   }).isRequired,
   setTask: PropTypes.func.isRequired,
 };
